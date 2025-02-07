5ea92f6d311f963cf632ac4a407b4b79f48e0eb9
Jun <51901925+missingems@users.noreply.github.com>
2025-02-07 20:21:58 +0800

    Initial Commit

---
+++ MouseTracking/MouseTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MouseTrackingApp.swift
+//  MouseTracking
+//
+//  Created by Jun on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct MouseTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
